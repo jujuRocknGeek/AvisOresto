3ba39d72487872e74c8febd32da64b87c8253004
Julien BELLAIS LAITHIER <jbellaislaithier@gmail.com>
2019-05-17 18:14:08 +0200

    amélioration TS et liste resto cliclable

--- src/interface.ts
+++ src/interface.ts
@@ -1,30 +1,80 @@
-/*place les markers et la liste des resto*/
+/************ initialise la carte**********/
+var maps:any;
+let mapRun = function():any{
+  return new Promise(function(resolve:any, reject:any){
+  navigator.geolocation.getCurrentPosition(function(position) {
+      var pos:any = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+      };
+  maps = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: pos});
 
-$.getJSON('restaurants.json',function(data:any){
-    $.each(data,function(index,d){
-      $('.list').append('<p>' + d.restaurantName +'<br>'+ d.address+'</p>');
-      $('p').attr('class', 'case'); 
-      
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(d.lat, d.long), 
-    map: map,
-    title: d.restaurantName
-        });
-    })
-});
+/* ajoute icone geolocalisation*/
+var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+var marker = new google.maps.Marker({
+  position: pos,
+  map: maps,
+  icon: iconBase + 'man.png'
+  });
+    });
 
- /* var infoWindowOptions = {
-    content: '<h3>Locronan</h3>'
-        + '<a href="http://www.locronan-tourisme.com/" target="_blank">Site de l office de tourisme de la ville</a>'
-        + '<br/><img src="maps/google-marker/image.jpg" width="200px" />'
+  if(navigator.geolocation){
+    resolve(maps);
+  }
+  else{
+    reject("pas de carte chargée");
+  }
+
+  });
 };
 
-var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+mapRun().then(function(map:any){
+  markerJson();
+},
+function(error: any){
+  console.log(error);
+});
+/********************** ************************************/
+/************ placer les marker google map JSON *************************/
+function markerJson(){
+  $.getJSON('restaurants.json',function(data){
+    $.each(data,function(index,d){
 
-google.maps.event.addListener(marker, 'click', function() {
-    infoWindow.open(map, marker);
-});*/
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(d.lat, d.long), 
+        map: maps,
+        title: d.restaurantName,
+        });
 
+            var infoWindowOptions = {
+              content: '<b>'+d.restaurantName+'</b>'+'<br>'+ d.address
+          };
+          
+          var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+            google.maps.event.addListener(marker, 'click', function() {
+          infoWindow.open(maps, marker);
+          });
 
+      $('.list').append('<p>' + d.restaurantName +'<br>'+ d.address +'</p>');
+      $('p').attr('class', 'case');
+    })
+  });
+}
 
+function open_infos()
+{
+        const width:number = 300;
+        const height:number = 200;
+        if(window.innerWidth)
+        {
+                var left = (window.innerWidth-width)/2;
+                var top = (window.innerHeight-height)/2;
+        }
+        else
+        {
+                var left = (document.body.clientWidth-width)/2;
+                var top = (document.body.clientHeight-height)/2;
+        }
+        window.open('inforesto.html','nom_de_ma_popup','menubar=no, scrollbars=no, top='+top+', left='+left+', width='+width+', height='+height+'');
+}
 
--- src/map.ts
+++ src/map.ts
@@ -1,57 +1,45 @@
-var infoWindow:any;
-var map:any = new google.maps.Map(
-  document.getElementById('map'), {zoom: 13, center: initMap().pos});
+/******** ouvrir une nouvelle fenetre au clic *********/
+$(document).ready(function(){
+var info:any;
 
-function initMap() {
-  infoWindow = new google.maps.InfoWindow;
-
-    if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-    var pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
-          
-/* ajoute icone geolocalisation*/
-var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-var marker = new google.maps.Marker({
-  position: pos,
-  map: map,
-  icon: iconBase + 'man.png'
+$(".case").click(function(){
+  info = $('p').html();
+  console.log(info);
+  /*open_infos();*/
+  PopupImage(info) 
 });
+})
 
-/*$.getJSON('restaurants.json',function(data:any){
-  $.each(data,function(index,d){
-    $('.list').append('<p>' + d.restaurantName +'<br>'+ d.address +'</p>');
-    $('p').attr('class', 'case'); 
-  var marker = new google.maps.Marker({
-  position: new google.maps.LatLng(d.lat, d.long), 
-  map: map,
-  title: d.restaurantName
-      });
-  })
-});*/
-
-
-  infoWindow.open(map);
-  map.setCenter(pos);
-  }, 
-  function() {
-  handleLocationError(true, infoWindow, map.getCenter());
-  });
-  } 
-  else {
-  // Browser doesn't support Geolocation
-  handleLocationError(false, infoWindow, map.getCenter());
-  }
-  return this.map;
+function PopupImage(info:any) 
+{ 
+  var titre:string;
+  titre="test fonction"; 
+  let w:any;
+	w=open("",'image','width=400,height=400,toolbar=no,scrollbars=no,resizable=no'); 
+	w.document.write("<HTML><HEAD><TITLE>"+titre+"</TITLE></HEAD>"); 
+	w.document.write("<SCRIPT language=javascript>function checksize() { if (document.images[0].complete) { window.resizeTo(document.images[0].width+30,document.images[0].height+150); window.focus();} else { setTimeout('checksize()',250) } }</"+"SCRIPT>"); 
+	w.document.write("<BODY onload='checksize()' onblur='window.close()' onclick='window.close()' leftMargin=0 topMargin=0 marginwidth=0 marginheight=0>");
+	w.document.write("<TABLE width='100%' border='0' cellspacing='0' cellpadding='0' height='100%'><TR>");
+  w.document.write("<TD valign='middle' align='center'>"); 
+  w.document.write('<p>'+ info +'</p>');
+	w.document.write("</TD></TR></TABLE>");
+	w.document.write("</BODY></HTML>"); 
+	w.document.close(); 
 }
 
-function handleLocationError(browserHasGeolocation:any, infoWindow:any, pos:any) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
+function open_infos()
+{
+        const width:number = 600;
+        const height:number = 800;
+        if(window.innerWidth)
+        {
+                var left = (window.innerWidth-width)/2;
+                var top = (window.innerHeight-height)/2;
+        }
+        else
+        {
+                var left = (document.body.clientWidth-width)/2;
+                var top = (document.body.clientHeight-height)/2;
+        }
+        window.open('inforesto.html','restarant nom','menubar=no, scrollbars=no, top='+top+', left='+left+', width='+width+', height='+height+'');
 }
-initMap();
