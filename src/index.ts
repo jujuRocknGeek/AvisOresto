e98bb4e879e964f387d82222bd20df88817bad8f
Julien BELLAIS LAITHIER <jbellaislaithier@gmail.com>
2019-05-20 08:53:03 +0200

    tentative resolution bug au clic

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
 require("../dist/assets/stylesheets/style.scss");
-require("./interface.ts");
 require("./map.ts");
+require("./interface.ts");
 console.log("test map.ts");
 });     
--- src/map.ts
+++ src/map.ts
@@ -1,16 +1,15 @@
 /******** ouvrir une nouvelle fenetre au clic *********/
 $(document).ready(function(){
 var info:any;
-
+console.log("je suis dans map.ts")
 $(".case").click(function(){
   info = $('p').html();
-  console.log(info);
-  /*open_infos();*/
+  console.log("tu as cliqué sur .case");
   PopupImage(info) 
 });
 })
 
-function PopupImage(info:any) 
+function PopupImage(detail:any) 
 { 
   var titre:string;
   titre="test fonction"; 
@@ -20,26 +19,10 @@ function PopupImage(info:any)
 	w.document.write("<SCRIPT language=javascript>function checksize() { if (document.images[0].complete) { window.resizeTo(document.images[0].width+30,document.images[0].height+150); window.focus();} else { setTimeout('checksize()',250) } }</"+"SCRIPT>"); 
 	w.document.write("<BODY onload='checksize()' onblur='window.close()' onclick='window.close()' leftMargin=0 topMargin=0 marginwidth=0 marginheight=0>");
 	w.document.write("<TABLE width='100%' border='0' cellspacing='0' cellpadding='0' height='100%'><TR>");
-  w.document.write("<TD valign='middle' align='center'>"); 
-  w.document.write('<p>'+ info +'</p>');
+        w.document.write("<TD valign='middle' align='center'>"); 
+        w.document.write('<p>'+ detail +'</p>');
 	w.document.write("</TD></TR></TABLE>");
 	w.document.write("</BODY></HTML>"); 
 	w.document.close(); 
 }
 
-function open_infos()
-{
-        const width:number = 600;
-        const height:number = 800;
-        if(window.innerWidth)
-        {
-                var left = (window.innerWidth-width)/2;
-                var top = (window.innerHeight-height)/2;
-        }
-        else
-        {
-                var left = (document.body.clientWidth-width)/2;
-                var top = (document.body.clientHeight-height)/2;
-        }
-        window.open('inforesto.html','restarant nom','menubar=no, scrollbars=no, top='+top+', left='+left+', width='+width+', height='+height+'');
-}
