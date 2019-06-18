6e846b6f811a94e5798d1fea83503f180cc9cb6b
Julien BELLAIS LAITHIER <jbellaislaithier@gmail.com>
2019-06-18 11:28:21 +0200

    configuration d'ajout de restaurant en cours

--- src/interface.ts
+++ src/interface.ts
@@ -1,9 +1,12 @@
 import { notDeepStrictEqual } from "assert";
 
 require("./rating.ts");
-var maps:any;
+var map:any;
 var marker:any;
-/*************************************************************/
+let jsonMarkers:any = [];
+let addRestoMarker:any = [];
+
+/***************************************************************************************/
 /********************* initialise la carte ******************/
 let mapRun = function(){
   return new Promise(function(resolve:any, reject:any){
@@ -12,36 +15,55 @@ let mapRun = function(){
       lat: position.coords.latitude,
       lng: position.coords.longitude
       };
-maps = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: pos});
+map = new google.maps.Map(document.getElementById('map'), {zoom: 13, center: pos});
 
-
-/*************************************************************/
 /*************** ajoute icone geolocalisation ***************/
 var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
 marker = new google.maps.Marker({
   position: pos,
-  map: maps,
+  map: map,
   icon: iconBase + 'man.png'
   });
-    });
+  return map;
+});
+
   if(navigator.geolocation){
     resolve();
   }
   else{
     reject("pas de carte chargée");
   }
-
   });
 };
-
+/***************************************************************************************/
   mapRun()
   .then(()=> setTimeout(() => {
-    markerJson(), list()
+    markerJson(), list(),addResto()
   }, 1000) )
   .catch((error)=>console.log(error))
+/***************************************************************************************/
 
 
-/************************************************************************/
+
+/***************************************************************************************/
+/************ Ajouter un resto au clic bouton ou map*************************/
+function addResto(){
+          // This event listener will call addMarker() when the map is clicked.
+          map.addListener('click', function(event:any) {
+            addMarker(event.latLng);
+          });
+  
+        // Adds a marker to the map and push to the array.
+        function addMarker(location:any) {
+          var marker = new google.maps.Marker({
+            position: location,
+            map: map
+          });
+          addRestoMarker.push(marker);
+        }
+}
+
+/***************************************************************************************/
 /************ placer les marker google map JSON *************************/
 function markerJson(){
   $.getJSON('restaurants.json',function(data){
@@ -50,7 +72,7 @@ function markerJson(){
       for (var i = 0; i < init.length; i++) {
         marker = new google.maps.Marker({
         position: new google.maps.LatLng(init[i].lat, init[i].long), 
-        map: maps,
+        map: map,
         title: init[i].restaurantName,
         });
 
@@ -60,15 +82,90 @@ function markerJson(){
           
           var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
             google.maps.event.addListener(marker, 'click', function() {
-          infoWindow.open(maps, marker);
+          infoWindow.open(map, marker);
           });
+
+          jsonMarkers.push(marker);
         }
     })
   });
+
+}
+/***************************************************************************************/
+
+
+
+/***************************************************************************************/
+/************ Filtre les restos par moyenne ******************/
+$(".selectAll").click(function(){
+  let starAverage = $("aside");
+  for(let i in starAverage){
+      let noteStars:string = $(`aside:eq(${i})`).html();
+      let noteNumber:number = parseInt(noteStars);
+        $(`article:eq(${i})`).show();
+        /*let titrerestoh5 = $(h5).html()
+        let titremarker = marker.gettitle();
+        if(titrerestoh5  === titremarker){ marker.setvisible(false)}*/
+
+  }  
+});
+
+$(".selectOne").click(selectOne);
+function selectOne(){
+  let starAverage = $("aside");
+  for(let i in starAverage){
+      let noteStars:string = $(`aside:eq(${i})`).html();
+      let noteNumber:number = parseInt(noteStars);
+      if(noteNumber > 1){
+        $(`article:eq(${i})`).hide();
+        console.log("dans la fonction")
+      }
+      else{
+          $(`article:eq(${i})`).show();
+          marker.setVisible(true);
+      }
+  }
 }
 
+$(".selectTwoThree").click(selectTwoThree);
+function selectTwoThree(){
+  let starAverage = $("aside");
+  for(let i in starAverage){
+      let noteStars:string = $(`aside:eq(${i})`).html();
+      let noteNumber:number = parseInt(noteStars);
+      if(noteNumber<2 || noteNumber>3){
+        $(`article:eq(${i})`).hide();
+      }
+      else{
+          $(`article:eq(${i})`).show();
+      }
+  }
+}
 
-/*************************************************************/
+$(".selectFourFive").click(selectFourFive);
+function selectFourFive(){
+  let starAverage = $("aside");
+  for(let i in starAverage){
+      let noteStars:string = $(`aside:eq(${i})`).html();
+      let noteNumber:number = parseInt(noteStars);
+      if(noteNumber < 4){
+        $(`article:eq(${i})`).hide();
+      }
+      else{
+          $(`article:eq(${i})`).show();
+      }
+  }
+}
+
+$("#select").change(function (){
+  $("#select option:selected").click();
+});
+/***************************************************************************************/
+
+
+
+
+/***************************************************************************************/
 /************ charge la requete AJAX *************************/
 function list(){
   var request = new XMLHttpRequest();
@@ -82,9 +179,11 @@ function list(){
       stars();
       average(resto);
     }
+/***************************************************************************************/
 
 
-/************************************************************/
+
+/***************************************************************************************/
 /************ Fonction affiche liste des restos ************/
   function loadResto(json:any){
 
@@ -100,6 +199,10 @@ function list(){
     var restoAddress = document.createElement('h6');
     var restoRating = document.createElement('ul');
     restoRating.setAttribute("class", "hidden");
+    var addStreetview = document.createElement('div');
+    addStreetview.setAttribute("class", "streetviewDiv");
+    var addImg = document.createElement('img');
+    addImg.src = `https://maps.googleapis.com/maps/api/streetview?size=200x100&location=${restos[i].lat},${restos[i].long}&fov=100&heading=235&pitch=10&key=AIzaSyC6_wqbr58IHAgGs3sBD1C934TSOYKX0lg`;
 
     restoName.textContent = restos[i].restaurantName;
     restoAddress.textContent = restos[i].address;
@@ -115,20 +218,27 @@ function list(){
       addContent.appendChild(addNote);
       addContent.appendChild(addComment);
       restoRating.appendChild(addContent);
+      addStreetview.appendChild(addImg);
+      console.log(restos[j].lat+","+restos[j].long);
     }
     
 
     list.appendChild(restoName);
     list.appendChild(restoAverage);
     list.appendChild(restoAddress);
+    list.appendChild(addStreetview);
     list.appendChild(restoRating);
     
     var section = document.querySelector(".list");
     section.appendChild(list);
 
   }
+/***************************************************************************************/
 
-/*************************************************************************/
+
+
+
+/***************************************************************************************/
 /************ Défini un lien et un Id a chaque Article et UL afin de pouvoir
                 afficher le détail des avis pour chaque resto ************/
   var refI=0;
@@ -165,8 +275,12 @@ function list(){
   }
 
 }
+/***************************************************************************************/
 
-/************************************************************/
+
+
+
+/***************************************************************************************/
 /********************** Objets Etoiles **********************/
 let etoiles= {
   one: "<img src='assets/img/1stars.png' id='stars'/>",
@@ -175,8 +289,12 @@ let etoiles= {
   four:"<img src='assets/img/4stars.png' id='stars'/>",
   five:"<img src='assets/img/5stars.png' id='stars'/>"
 };
+/***************************************************************************************/
 
-/************************************************************/
+
+
+
+/***************************************************************************************/
 /******* Fonction affiche images "etoiles" des notes *******/
 function stars(){
   let i;
@@ -206,8 +324,12 @@ function stars(){
   }
 
 }
+/***************************************************************************************/
 
-/************************************************************/
+
+
+
+/***************************************************************************************/
 /******* Fonction affiche la moyenne des restaurants *******/
 function average(json:any){
   let nbRestos = json[0]['restos'];
@@ -225,36 +347,113 @@ function average(json:any){
     $(`aside:eq(${i})`).append(moyenne)
   }
 }
+/***************************************************************************************/
 
-$(".selectAll").click(function(){
-  console.log("clic sur resto tous les restos");
-});
 
-$(".selectOne").click(selectOne);
-function selectOne(){
-  console.log("clic sur resto 0 1 etoile");
-  let starAverage = $("aside");
-  for(let i in starAverage){
-    let noteStars = $(`aside:eq(${i})`).html();
-  }
-}
+/***************************************************************************************/
+/***************  Recupère Infos Formulaire d'ajout de restaurants  ********************/
+$("#submitBtn").click(()=>{
+  console.log("coucou");
+  var restoFormName:any = $("#nom").val();
+  var restoFormAdress:any = $("#adresse").val();
+  var restoFormNote:any = $("#rate").val();
+  var restoFormAvis:any = $("#avis").val();
 
-$(".selectTwoThree").click(selectTwoThree);
-function selectTwoThree(){
-  console.log("clic sur resto 2 3 etoile");
-  let starAverage = $("aside");
-  for(let i in starAverage){
-    let noteStars = $(`aside:eq(${i})`).html();
-  }
-}
 
-$(".selectFourFive").click(selectFourFive);
-function selectFourFive(){
-  console.log("clic sur resto 4 5 etoile");
-  let starAverage = $("aside");
-  for(let i in starAverage){
-    let noteStars = $(`aside:eq(${i})`).html();
-  }
-}
+  var list = document.createElement('article');
+  list.setAttribute("class", "case");
+  list.setAttribute("id", "caseAdd");
+  list.setAttribute("data-js", "hide");
+  var restoName = document.createElement('h5');
+  var restoAverage = document.createElement('aside');
+  restoAverage.setAttribute("class", "averageBox");
+  var restoAddress = document.createElement('h6');
+  var restoRating = document.createElement('ul');
+  restoRating.setAttribute("class", "hidden");
+  restoRating.setAttribute("id", "ulAdd");
+  var addStreetview = document.createElement('div');
+  addStreetview.setAttribute("class", "streetviewDiv");
+  var addImg = document.createElement('img');
+  addImg.src = `https://maps.googleapis.com/maps/api/streetview?size=200x100&location=${restoFormAdress}&fov=100&heading=235&pitch=10&key=AIzaSyC6_wqbr58IHAgGs3sBD1C934TSOYKX0lg`;
+  var addNote = document.createElement('li');
+  addNote.setAttribute("class", "liAdd");
+  var addComment = document.createElement('li');
+  var addContent = document.createElement('div');
+  var addContentClass = addContent.setAttribute("class", "ratings");
+
+  restoName.append(restoFormName);
+  restoAddress.append(restoFormAdress);
+  addStreetview.append(addImg);
+  addNote.append(restoFormNote);
+  addComment.append(restoFormAvis)
+  addContent.appendChild(addNote);
+  addContent.appendChild(addComment);
+  restoRating.appendChild(addContent);
+
+  list.appendChild(restoName);
+  list.appendChild(restoAverage);
+  list.appendChild(restoAddress);
+  list.appendChild(addStreetview);
+  list.appendChild(restoRating);
+  console.log(restoName);
+
+  var section = document.querySelector(".list");
+  section.appendChild(list);
+
+  $("#caseAdd").click(function (event:any) {
+    var hiderTarget = document.querySelector('ul[id="ulAdd"]')
+    hiderTarget.classList.toggle('-visible');
+    console.log("coucou ul")
+    });
+
+    let note = $('li[class="liAdd"]').html();
+    console.log(note)
+    switch(note){
+      case "0": 
+      $('li[class="liAdd"]').html(etoiles.one);
+      break;
+      case "1": 
+      $('li[class="liAdd"]').html(etoiles.one);
+      break;
+      case "2": 
+      $('li[class="liAdd"]').html(etoiles.two);
+      break;
+      case "3": 
+      $('li[class="liAdd"]').html(etoiles.three);
+      break;
+      case "4": 
+      $('li[class="liAdd"]').html(etoiles.four);
+      break;
+      case "5": 
+      $('li[class="liAdd"]').html(etoiles.five);
+      break;
+    }
+  
+
+  /*var addNote = document.createElement('li');
+  var addComment = document.createElement('li');
+  var addContent = document.createElement('div');
+  var addContentClass = addContent.setAttribute("class", "ratings");
+  addNote.textContent = rating[j].stars;
+  addComment.textContent =  rating[j].comment;
+  addContent.appendChild(addNote);
+  addContent.appendChild(addComment);
+  restoRating.appendChild(addContent);
+  addStreetview.appendChild(addImg);
+  console.log(restos[j].lat+","+restos[j].long);
+
+  list.appendChild(restoName);
+  list.appendChild(restoAverage);
+  list.appendChild(restoAddress);
+  list.appendChild(addStreetview);
+  list.appendChild(restoRating);
+  
+  var section = document.querySelector(".list");
+  section.appendChild(list);*/
+
+})
+
+
+/***************************************************************************************/
 /*Object.keys().length   console.log(Object.keys(json[0]['restos']).length) */
 
